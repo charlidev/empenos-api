4877fd048fd0120a4b0004b12a8db0ca4751c2b5
charliedev <carlos.martinez.cbtis59@gmail.com>
2025-06-10 17:15:19 -0600

    feat: agrega frontend en carpeta public

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+//importando express
 const express = require('express');
 const app = express();
-const prestamosRoutes = require('./routes/prestamos');
+const path = require('path');
 
-app.use(express.json());
+// Importando las rutas de prestamos
+const prestamosRoutes = require('./routes/prestamos');
 app.use('/api', prestamosRoutes);
 
+// Accede a la carpeta 'public' para servir archivos estáticos
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(express.json());
+
+// levantando el servidor en puerto 3000
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
